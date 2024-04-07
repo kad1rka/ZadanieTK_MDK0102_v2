9f678cb95e53565ad8f45c9d35b176cc0c71048a
Никита Рубцов <kadirmc@mail.ru>
2024-04-07 17:23:54 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ZadanieTKExam;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SuccesMarks()
+        {
+            var page = new Page1();
+            Assert.IsTrue(page.CheckMarks("20","20","20") == 5);
+            Assert.IsTrue(page.CheckMarks("15", "15", "15") == 4);
+            Assert.IsTrue(page.CheckMarks("8", "5", "10") == 3);
+            Assert.IsTrue(page.CheckMarks("3", "3","3") == 2);
+            Assert.IsTrue(page.CheckMarks("0", "0", "0") == 2);
+
+        }
+        [TestMethod]
+        public void UnsuccesMarks()
+        {
+            var page = new Page1();
+            Assert.IsTrue(page.CheckMarks("-12", "0", "3") == -1);
+            Assert.IsTrue(page.CheckMarks("23", "10", "10") == -1);
+            Assert.IsTrue(page.CheckMarks("10", "39", "10") == -1);
+            Assert.IsTrue(page.CheckMarks("10","10","21") == -1);
+            Assert.IsTrue(page.CheckMarks("", "", "") == -1);
+            Assert.IsTrue(page.CheckMarks("-2","10","10") == -1);
+            Assert.IsTrue(page.CheckMarks("10", "-1", "10") == -1);
+            Assert.IsTrue(page.CheckMarks("10", "10", "-1") == -1);
+            
+        }
+    }
+}
---
+++ ZadanieTKExam/Page1.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZadanieTKExam
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        public int CheckMarks(string FirstMark, string SecondMark, string ThirdMark)
+        {
+
+            int intFirstMark, intSecondMark, intThirdMark;
+
+            if (!int.TryParse(FirstMark, out intFirstMark) || !int.TryParse(SecondMark, out intSecondMark) || !int.TryParse(ThirdMark, out intThirdMark))
+            {
+
+                MessageBox.Show("Ошибка ввода оценок!");
+                return -1;
+            }
+            if (intFirstMark < 0 || intFirstMark > 22 || intSecondMark < 0 || intSecondMark > 38 || intThirdMark < 0 || intThirdMark > 20)
+            {
+                MessageBox.Show("Выход за границы баллов.");
+                return -1;
+            }
+            int SumMark = intFirstMark + intSecondMark + intThirdMark;
+            if (SumMark > +56)
+            {
+                return 5;
+            }
+            else if (SumMark > 32)
+            {
+                return 4;
+            }
+            else if (SumMark > 16)
+            {
+                return 3;
+
+            }
+            else if (SumMark >= 0 || SumMark <= 16)
+            {
+                return 2;
+            }
+            else { return -1; }
+
+
+
+        }
+        private void Execute_Click(object sender, RoutedEventArgs e)
+        {
+            Mark.Content = $"Оценка: {CheckMarks(FirstMark.Text, SecondMark.Text, ThirdMark.Text)}";
+
+        }
+    }
+}
